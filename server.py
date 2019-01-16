df4236aa6a30c0e2c442ccf2540ab38957ac29ae
Katalin Csortos <katalin.csortos@gmail.com>
2019-01-16 10:39:59 +0100

    feat: added player dataset variables to server

--- server.py
+++ server.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('game.html')
+    player_one_start_pos = 3
+    player_one_can_move = 'false'
+    player_one_start_hp = 100
+
+    player_two_start_pos = 82
+    player_two_can_move = 'false'
+    player_two_start_hp = 100
+
+    return render_template('game.html', p1_position=player_one_start_pos, p2_position=player_two_start_pos,
+                           p1_can_move=player_one_can_move, p2_can_move=player_two_can_move,
+                           p1_start_hp=player_one_start_hp, p2_start_hp=player_two_start_hp)
 
 
 if __name__ == '__main__':
