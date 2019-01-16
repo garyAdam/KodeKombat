f06aebf69dbbb4accb720a82dc2bd4d734f03559
AndrasSzemes <andras@szemes.eu>
2019-01-16 21:20:52 +0100

    Merge dev branch into feat/player-movement

--- static/js/game.js
+++ static/js/game.js
@@ -1,151 +1,11 @@
-// let playerMovement = function (event) {
-//     let movementDistance = 0.7;
-//
-//     let playerOne = document.getElementById('player-one');
-//     let playerOnePosition = parseFloat(playerOne.dataset.playerPosition);
-//     let playerOneCanMoveOrAttack = playerOne.dataset.canMoveOrAttack;
-//
-//     let playerTwo = document.getElementById('player-two');
-//     let playerTwoPosition = parseFloat(playerTwo.dataset.playerPosition);
-//     let playerTwoCanMoveOrAttack = playerTwo.dataset.canMoveOrAttack;
-//
-//     /*
-//     KEY CODES:
-//
-//     65 - a
-//     68 - d
-//     69 - e
-//     74 - j
-//     76 - l
-//     85 - u
-//
-//      */
-//
-//     if (event.keyCode === 68) {
-//         movePlayer(playerOne, 'right', movementDistance);
-//     }
-//     if (event.keyCode === 65) {
-//         movePlayer(playerOne, 'left', movementDistance);
-//     }
-//
-//     if (event.keyCode === 69) {
-//         if (playerOneCanMoveOrAttack === 'false') {
-//             playerOne.src = "/static/images/jin_punch.gif";
-//             playerOne.dataset.canMoveOrAttack = 'true';
-//             if (playerTwoPosition - playerOnePosition < 17){
-//                 playerTwo.dataset.hp -= 10;
-//                 updateHPBar('player-two');
-//         }
-//         }
-//     }
-//
-//     if (event.keyCode === 74) {
-//         movePlayer(playerTwo, 'left', movementDistance);
-//     }
-//
-//     if (event.keyCode === 76) {
-//         movePlayer(playerTwo, 'right', movementDistance);
-//     }
-//
-//     if (event.keyCode === 85) {
-//         if (playerTwoCanMoveOrAttack === 'false') {
-//             playerTwo.src = "/static/images/asuka_punch_new.gif";
-//             playerTwo.dataset.canMoveOrAttack = 'true';
-//             if (playerTwoPosition - playerOnePosition < 12){
-//                 playerOne.dataset.hp -= 10;
-//                 updateHPBar('player-one');
-//         }
-//     }}
-//
-//     function movePlayer(player, direction, movementDistance) {
-//         if (player === playerOne) {
-//             if (direction === 'right' && playerOnePosition < playerTwoPosition - 10) {
-//                 playerOnePosition += movementDistance;
-//             } else if (direction === 'left' && playerOnePosition > 3) {
-//                 playerOnePosition -= movementDistance;
-//             }
-//
-//             player.style.left = playerOnePosition + "%";
-//             player.dataset.playerPosition = playerOnePosition;
-//
-//             if (playerOneCanMoveOrAttack === 'false') {
-//                 if (direction === 'right') {
-//                     player.src = "/static/images/jin_walk.gif";
-//                     player.dataset.canMoveOrAttack = 'true';
-//                 } else if (direction === 'left') {
-//                     player.src = "/static/images/jin_backwalk.gif";
-//                     player.dataset.canMoveOrAttack = 'true';
-//                 }
-//             }
-//         } else {
-//             if (direction === 'right' && playerTwoPosition < 87) {
-//                 playerTwoPosition += movementDistance;
-//             } else if (direction === 'left' && playerOnePosition +10 < playerTwoPosition) {
-//                 playerTwoPosition -= movementDistance;
-//             }
-//
-//
-//             player.style.left = playerTwoPosition + "%";
-//             player.dataset.playerPosition = playerTwoPosition;
-//
-//             if (playerTwoCanMoveOrAttack === 'false') {
-//                 if (direction === 'left') {
-//                     player.src = "/static/images/asuka_walk.gif";
-//                     player.dataset.canMoveOrAttack = 'true';
-//                 } else if (direction === 'right') {
-//                     player.src = "/static/images/asuka_backwalk.gif";
-//                     player.dataset.canMoveOrAttack = 'true';
-//                 }
-//
-//             }
-//         }
-//
-//     }
-//
-// };
-//
-//
-//
-//
-// let playerOneBackToStance = function () {
-//     let playerOne = document.getElementById('player-one');
-//     setTimeout(function () {
-//         playerOne.src = "/static/images/jin_stance.gif";
-//         playerOne.dataset.canMoveOrAttack = 'false';
-//
-//     }, 500);
-//
-//
-// };
-//
-//
-// let playerTwoBackToStance = function () {
-//     let playerTwo = document.getElementById('player-two');
-//     playerTwo.src = "/static/images/asuka_stance.gif";
-//     playerTwo.dataset.canMoveOrAttack = 'false';
-//
-// };
-//
-//
-// let init = function () {
-//     addEventListener('keydown', playerMovement, false);
-//     addEventListener('keyup', playerOneBackToStance, false);
-//     addEventListener('keyup', playerTwoBackToStance, false);
-// };
-//
-//
-// let updateHPBar = function(playerID) {
-//     let player = document.getElementById(playerID);
-//     let playerActualHP = player.dataset.hp;
-//
-//     let playerHPbar = document.getElementById(`${playerID}-HP`).firstElementChild;
-//     playerHPbar.style.width = (parseInt(playerActualHP) * 0.3) + 'vw';
-// };
-//
-//
-// init();
-//
-//
+let updateHPBar = function(playerID) {
+    let player = document.getElementById(playerID);
+    let playerActualHP = player.dataset.hp;
+
+    let playerHPbar = document.getElementById(`${playerID}-HP`).firstElementChild;
+    playerHPbar.style.width = (parseInt(playerActualHP) * 0.3) + 'vw';
+};
+
 
 let playerBackToStance = function (player, stanceAnimation) {
     let playerElement = document.getElementById(player);
@@ -165,6 +25,9 @@ let init = function () {
     let playerTwo = document.getElementById('player-two');
     let playerTwoPosition = parseFloat(playerTwo.dataset.playerPosition);
     let playerTwoCanMoveOrAttack = playerTwo.dataset.canMoveOrAttack;
+
+    let playerOneStartPosition = 3;
+    let playerTwoStartPosition = 82;
 
     let keysDown = {};
 
@@ -192,6 +55,12 @@ let init = function () {
 
 
     addEventListener("keydown", function (event) {
+        let playersNotCrossing = playerOnePosition + 10 < playerTwoPosition;
+        let playerOneStayInWindow = playerOnePosition > playerOneStartPosition;
+        let playerTwoStayInWindow = playerTwoPosition < playerTwoStartPosition;
+        let playerTwoInPunchRange = playerTwoPosition - playerOnePosition < 17;
+        let playerOneInPunchRange = playerTwoPosition - playerOnePosition < 12;
+
         let pressedKey = event.keyCode;
         for (const key of keyAnimationsP1.keys()) {
             if (pressedKey === key && !keysDown[key]) {
@@ -213,13 +82,13 @@ let init = function () {
 
         keysDown[pressedKey] = true;
 
-        if (dKey in keysDown) { // P1 pressed walk to the right (key: d), key code: 68
+        if (dKey in keysDown && playersNotCrossing) { // P1 pressed walk to the right (key: d), key code: 68
             playerOnePosition += movementDistance;
             playerOne.dataset.playerPosition = playerOnePosition;
             playerOne.style.left = playerOnePosition + "%";
         }
 
-        if (aKey in keysDown) { // P1 pressed walk to the left (key: a), key code: 65
+        if (aKey in keysDown && playerOneStayInWindow) { // P1 pressed walk to the left (key: a), key code: 65
             playerOnePosition += -(movementDistance);
             playerOne.dataset.playerPosition = playerOnePosition;
             playerOne.style.left = playerOnePosition + "%";
@@ -227,15 +96,19 @@ let init = function () {
 
         if (eKey in keysDown) { // P1 pressed punch (key: e), key code: 69
             playerOne.dataset.canMoveOrAttack = 'true';
+            if (playerTwoInPunchRange) {
+                playerTwo.dataset.hp -= 10;
+                updateHPBar('player-two');
+            }
         }
 
-        if (jKey in keysDown) { // P2 pressed walk to the left (key: j), key code: 74
+        if (jKey in keysDown && playersNotCrossing) { // P2 pressed walk to the left (key: j), key code: 74
             playerTwoPosition += -(movementDistance);
             playerTwo.dataset.playerPosition = playerTwoPosition;
             playerTwo.style.left = playerTwoPosition + "%";
         }
 
-        if (lKey in keysDown) { // P2 pressed walk to the right (key: l), key code 76
+        if (lKey in keysDown && playerTwoStayInWindow) { // P2 pressed walk to the right (key: l), key code 76
             playerTwoPosition += movementDistance;
             playerTwo.dataset.playerPosition = playerTwoPosition;
             playerTwo.style.left = playerTwoPosition + "%";
@@ -243,6 +116,10 @@ let init = function () {
 
         if (uKey in keysDown) { // P2 pressed punch (key: u), key code: 85
             playerTwo.dataset.canMoveOrAttack = 'true';
+            if (playerOneInPunchRange) {
+                playerOne.dataset.hp -= 10;
+                updateHPBar('player-one');
+            }
         }
     }, false);
 
